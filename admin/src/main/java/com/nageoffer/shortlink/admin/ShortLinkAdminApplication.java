5dec792400345c3dd659430e6e023a17d9ad12ca
linqi <wkw4399xs@gmail.com>
2023-11-05 15:16:38 +0800

    Third commit: Start and build the admin module of the project, build the configuration files of the project, gateway, and admin module, and introduce the SpringBoot-starter-web file of pam. xml.

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,19 @@
+package com.nageoffer.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ShortLinkAdminApplication
+ * @Description
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/11/05
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class,args);
+    }
+}
