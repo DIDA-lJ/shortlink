21d3423d2bbeae14228f60908445e10375775540
linqi <wkw4399xs@gmail.com>
2023-11-17 03:52:38 +0800

    Seventh commit: Unified global return entity object encapsulation.

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import com.nageoffer.shortlink.admin.convention.result.Result;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -28,8 +29,10 @@ public class UserController {
      * 根据用户名查询用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
-        return userService.getUserByUsername(username);
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        UserRespDTO result = userService.getUserByUsername(username);
+        return new Result<UserRespDTO>().setCode("0").
+                setData(userService.getUserByUsername(username));
     }
 
 }
---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/convention/result/Result.java
@@ -0,0 +1,51 @@
+package com.nageoffer.shortlink.admin.convention.result;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * @ClassName Result
+ * @Description 全局返回对象
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/11/07
+ */
+@Data
+@Accessors(chain = true)
+public class Result<T> implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 5679018624309023727L;
+
+    /**
+     * 正确返回码
+     */
+    public static final String SUCCESS_CODE = "0";
+
+    /**
+     * 返回码
+     */
+    private String code;
+
+    /**
+     * 返回消息
+     */
+    private String message;
+
+    /**
+     * 响应数据
+     */
+    private T data;
+
+    /**
+     * 请求ID
+     */
+    private String requestId;
+
+    public boolean isSuccess() {
+        return SUCCESS_CODE.equals(code);
+    }
+}
