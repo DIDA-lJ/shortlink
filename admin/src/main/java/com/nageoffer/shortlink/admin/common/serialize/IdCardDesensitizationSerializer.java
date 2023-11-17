864052543212e681794a6e4b6ff8949ba20afb89
linqi <wkw4399xs@gmail.com>
2023-11-17 22:51:25 +0800

    11th commit:  Id card desensitization serialization

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/serialize/IdCardDesensitizationSerializer.java
@@ -0,0 +1,25 @@
+package com.nageoffer.shortlink.admin.common.serialize;
+
+import cn.hutool.core.util.DesensitizedUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+/**
+ * @ClassName IdCardDesensitizationSerializer
+ * @Description 身份证号脱敏反序列化
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/11/17
+ */
+
+public class IdCardDesensitizationSerializer extends JsonSerializer<String> {
+
+    @Override
+    public void serialize(String idCard, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        String phoneDesensitization = DesensitizedUtil.idCardNum(idCard, 4, 4);
+        jsonGenerator.writeString(phoneDesensitization);
+    }
+}
