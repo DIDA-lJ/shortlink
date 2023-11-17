167698bdd9848753e56b34f6241198af692c5e10
linqi <wkw4399xs@gmail.com>
2023-11-17 22:42:24 +0800

    Tenth commit:  UserController Search user information based on the user name interface improved.

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -2,7 +2,6 @@ package com.nageoffer.shortlink.admin.controller;
 
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
-import com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -32,14 +31,7 @@ public class UserController {
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
-        UserRespDTO result = userService.getUserByUsername(username);
-        if(result == null){
-            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).
-                    setMessage(UserErrorCodeEnum.USER_NULL.message());
-        }else{
-            return Results.success(result);
-        }
-
+        return Results.success(userService.getUserByUsername(username));
     }
 
 }
