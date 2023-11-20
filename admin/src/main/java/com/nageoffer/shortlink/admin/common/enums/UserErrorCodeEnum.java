2fbfc7d7d2b7fc785992ee76429aeb3b80011597
linqi <wkw4399xs@gmail.com>
2023-11-20 23:31:18 +0800

    The 21th commit(feature):The user exception code is added.

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -11,9 +11,15 @@ import com.nageoffer.shortlink.admin.common.convention.errorcode.IErrorCode;
  */
 public enum UserErrorCodeEnum implements IErrorCode {
 
-    USER_NULL("B000200","用户不存在"),
+    USER_TOKEN_FAIL("A000200", "用户Token验证失败"),
 
-    USER_EXIST("B000201","用户已经存在");
+    USER_NULL("B000200", "用户记录不存在"),
+
+    USER_NAME_EXIST("B000201", "用户名已存在"),
+
+    USER_EXIST("B000202", "用户记录已存在"),
+
+    USER_SAVE_ERROR("B000203", "用户记录新增失败");
 
     private final String code;
 
