cc6ab68188ee5d4590060a2e0a43fa04ba7e40a8
linqi <wkw4399xs@gmail.com>
2023-11-20 23:33:39 +0800

    The 21th commit(feature):Database persistence layer configuration class created.

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,28 @@
+package com.nageoffer.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName DataBaseConfiguration
+ * @Description 数据库持久层配置类
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/11/20
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
