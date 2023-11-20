d8c8a03de2d876e4e7704354f4c09442f4141170
linqi <wkw4399xs@gmail.com>
2023-11-20 23:36:05 +0800

    The 23th commit(feature):Database persistence layer object base property creation.

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/database/BaseDO.java
@@ -0,0 +1,36 @@
+package com.nageoffer.shortlink.admin.common.database;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @ClassName BaseDO
+ * @Description 数据库持久层对象基础属性
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/11/20
+ */
+@Data
+public class BaseDO {
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
