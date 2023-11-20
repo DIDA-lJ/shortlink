ddb7ecf2fd73842d82055c8e98d114979666c0d9
linqi <wkw4399xs@gmail.com>
2023-11-20 23:34:53 +0800

    The 22th commit(feature):Short link background manages Redis cache constant classes.

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,15 @@
+package com.nageoffer.shortlink.admin.common.constant;
+
+/**
+ * @ClassName RedisCacheConstant
+ * @Description 短链接后管 Redis 缓存常量类
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/11/20
+ */
+public class RedisCacheConstant {
+    /**
+     * 短链接后管 Redis 缓存常量类
+     */
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
