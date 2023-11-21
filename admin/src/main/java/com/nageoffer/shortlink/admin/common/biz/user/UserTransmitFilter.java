5171c40c2811891207e1f82a944a36267bcccafe
linqi <wkw4399xs@gmail.com>
2023-11-21 23:23:10 +0800

    The 26th commit(feature): blank class template creation.

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -0,0 +1,18 @@
+package com.nageoffer.shortlink.admin.common.biz.user;
+
+
+import lombok.RequiredArgsConstructor;
+
+
+/**
+ * @ClassName UserTransmitFilter
+ * @Description
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/11/20
+ */
+@RequiredArgsConstructor
+public class UserTransmitFilter  {
+
+
+}
---
+++ admin/src/test/java/UserTableShardingTest.java
@@ -0,0 +1,9 @@
+/**
+ * @ClassName UserTableShardingTest
+ * @Description
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/11/20
+ */
+public class UserTableShardingTest {
+}
