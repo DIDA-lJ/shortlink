a678aadee2807f1d51c67f33e43878d3fc366c74
linqi <wkw4399xs@gmail.com>
2023-11-28 03:29:05 +0800

    The 36th commit(feature): Creates a short link grouping entity

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/GroupDO.java
@@ -0,0 +1,48 @@
+package com.nageoffer.shortlink.admin.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.nageoffer.shortlink.admin.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @ClassName GroupDO
+ * @Description 短链接分组实体
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/11/28
+ */
+@Data
+@TableName("t_group")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 分组名称
+     */
+    private String name;
+
+    /**
+     * 创建分组用户名
+     */
+    private String username;
+
+    /**
+     * 分组排序
+     */
+    private Integer sortOrder;
+}
