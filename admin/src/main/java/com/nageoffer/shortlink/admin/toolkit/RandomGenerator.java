8108dfc53ea73fc4426c840bb5c6b04dba725780
linqi <wkw4399xs@gmail.com>
2023-11-28 03:50:27 +0800

    The 40th commit(feature): The compilation of tool group ID random generator.

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/toolkit/RandomGenerator.java
@@ -0,0 +1,41 @@
+package com.nageoffer.shortlink.admin.toolkit;
+
+/**
+ * @ClassName RandomGenerator
+ * @Description 分组ID随机生成器
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/11/28
+ */
+
+import java.security.SecureRandom;
+
+public final class RandomGenerator {
+
+    private static final String CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    /**
+     * 生成随机分组ID
+     *
+     * @return 分组ID
+     */
+    public static String generateRandom() {
+        return generateRandom(6);
+    }
+
+    /**
+     * 生成随机分组ID
+     *
+     * @param length 生成多少位
+     * @return 分组ID
+     */
+    public static String generateRandom(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = RANDOM.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
+}
