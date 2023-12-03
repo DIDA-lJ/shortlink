e268a12bca66925751d21cee2b0a7e5c5e84ebfe
linqi <wkw4399xs@gmail.com>
2023-12-03 22:24:28 +0800

    The 59th commit(feature): Added code file open source protocol header.(增加代码文件开源协议头。)

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/convention/errorcode/IErrorCode.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/convention/errorcode/IErrorCode.java
@@ -28,11 +28,13 @@ public interface IErrorCode {
 
     /**
      * 错误码
+     * @return 错误码
      */
     String code();
 
     /**
      * 错误信息
+     * @return 错误信息
      */
     String message();
 }
