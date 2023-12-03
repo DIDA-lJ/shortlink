e9c79943aed50cb4ca85284f3a5f1f61b4ff48b4
linqi <wkw4399xs@gmail.com>
2023-12-03 22:21:40 +0800

    The 56th commit(feature): Added code file open source protocol header.(增加代码文件开源协议头。)

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -1,5 +1,3 @@
-package com.nageoffer.shortlink.admin.common.constant;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,10 @@ package com.nageoffer.shortlink.admin.common.constant;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.nageoffer.shortlink.admin.common.constant;
+
+
 
 
 /**
