f033f4231d82c495de4da4c0d95b033fce1cd5bb
linqi <wkw4399xs@gmail.com>
2023-12-03 22:45:02 +0800

    The 70th commit(feature): Project module Redis Key constant class development.(Project 模块 Redis Key 常量类.)

---
+++ project/src/main/java/com/nageoffer/shorklink/project/common/constant/RedisKeyConstant.java
@@ -0,0 +1,36 @@
+package com.nageoffer.shorklink.project.common.constant;
+
+/**
+ * @ClassName RedisKeyConstant
+ * @Description  Redis Key 常量类
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/12/03
+ */
+public class RedisKeyConstant {
+
+    /**
+     * 短链接跳转前缀 Key
+     */
+    public static final String GOTO_SHORT_LINK_KEY = "short-link_goto_%s";
+
+    /**
+     * 短链接空值跳转前缀 Key
+     */
+    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short-link_is-null_goto_%s";
+
+    /**
+     * 短链接跳转锁前缀 Key
+     */
+    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link_lock_goto_%s";
+
+    /**
+     * 短链接修改分组 ID 锁前缀 Key
+     */
+    public static final String LOCK_GID_UPDATE_KEY = "short-link_lock_update-gid_%s";
+
+    /**
+     * 短链接延迟队列消费统计 Key
+     */
+    public static final String DELAY_QUEUE_STATS_KEY = "short-link_delay-queue:stats";
+}
