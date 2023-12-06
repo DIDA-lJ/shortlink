fbab137b56eeade9a3319835d7dde58102a3b795
linqi <wkw4399xs@gmail.com>
2023-12-06 21:17:25 +0800

    The 75th commit(feature): Project module remote call exception.(Project 模块 远程调用异常.)

---
+++ project/src/main/java/com/nageoffer/shorklink/project/common/convention/exception/RemoteException.java
@@ -0,0 +1,34 @@
+package com.nageoffer.shorklink.project.common.convention.exception;
+
+import com.nageoffer.shorklink.project.common.convention.errorcode.BaseErrorCode;
+import com.nageoffer.shorklink.project.common.convention.errorcode.IErrorCode;
+
+/**
+ * @ClassName RemoteException
+ * @Description 远程服务调用异常
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/12/06
+ */
+public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
+
+    public RemoteException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public RemoteException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "RemoteException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
