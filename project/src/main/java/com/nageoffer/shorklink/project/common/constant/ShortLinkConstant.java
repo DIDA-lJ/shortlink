ee774c76120a91ed3b5dbb556a378f44d3320b46
linqi <wkw4399xs@gmail.com>
2023-12-06 20:34:22 +0800

    The 71th commit(feature): Project module short connection module constant class writing.(Project 模块 短连接模块 常量类编写.)

---
+++ project/src/main/java/com/nageoffer/shorklink/project/common/constant/ShortLinkConstant.java
@@ -0,0 +1,21 @@
+package com.nageoffer.shorklink.project.common.constant;
+
+/**
+ * @ClassName ShortLinkConstant
+ * @Description 短链接常量类
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/12/06
+ */
+public class ShortLinkConstant {
+
+    /**
+     * 永久短链接默认缓存有效时间，默认一个月
+     */
+    public static final long DEFAULT_CACHE_VALID_TIME = 2626560000L;
+
+    /**
+     * 高德获取地区接口地址
+     */
+    public static final String AMAP_REMOTE_URL = "https://restapi.amap.com/v3/ip";
+}
