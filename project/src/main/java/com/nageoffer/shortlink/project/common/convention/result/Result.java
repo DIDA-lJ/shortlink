20cc76abaf6760f45388b11e5964b62d6285bb82
linqi <wkw4399xs@gmail.com>
2023-12-06 22:40:38 +0800

    The 80th commit(feature): Project module Supplementing the pom.xml dependency..(Project 全局返回对象修改.)

--- project/src/main/java/com/nageoffer/shortlink/project/common/convention/result/Result.java
+++ project/src/main/java/com/nageoffer/shortlink/project/common/convention/result/Result.java
@@ -1,4 +1,4 @@
-package com.nageoffer.shorklink.project.common.convention.result;
+package com.nageoffer.shortlink.project.common.convention.result;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
