9dee7d16339afa51d2caca9c52da2fc8e3eeece6
linqi <wkw4399xs@gmail.com>
2023-12-06 22:23:46 +0800

    The 77th commit(feature): Project module Global return object constructor creation.(Project 模块 全局返回对象构造器.)

---
+++ project/src/main/java/com/nageoffer/shorklink/project/common/convention/result/Results.java
@@ -0,0 +1,64 @@
+package com.nageoffer.shorklink.project.common.convention.result;
+
+import com.nageoffer.shorklink.project.common.convention.errorcode.BaseErrorCode;
+import com.nageoffer.shorklink.project.common.convention.exception.AbstractException;
+
+import java.util.Optional;
+
+/**
+ * @ClassName Resilts
+ * @Description 全局返回对象构造器
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/12/06
+ */
+public final class Results {
+
+    /**
+     * 构造成功响应
+     */
+    public static Result<Void> success() {
+        return new Result<Void>()
+                .setCode(Result.SUCCESS_CODE);
+    }
+
+    /**
+     * 构造带返回数据的成功响应
+     */
+    public static <T> Result<T> success(T data) {
+        return new Result<T>()
+                .setCode(Result.SUCCESS_CODE)
+                .setData(data);
+    }
+
+    /**
+     * 构建服务端失败响应
+     */
+    public static Result<Void> failure() {
+        return new Result<Void>()
+                .setCode(BaseErrorCode.SERVICE_ERROR.code())
+                .setMessage(BaseErrorCode.SERVICE_ERROR.message());
+    }
+
+    /**
+     * 通过 {@link AbstractException} 构建失败响应
+     */
+    public static Result<Void> failure(AbstractException abstractException) {
+        String errorCode = Optional.ofNullable(abstractException.getErrorCode())
+                .orElse(BaseErrorCode.SERVICE_ERROR.code());
+        String errorMessage = Optional.ofNullable(abstractException.getErrorMessage())
+                .orElse(BaseErrorCode.SERVICE_ERROR.message());
+        return new Result<Void>()
+                .setCode(errorCode)
+                .setMessage(errorMessage);
+    }
+
+    /**
+     * 通过 errorCode、errorMessage 构建失败响应
+     */
+    public static Result<Void> failure(String errorCode, String errorMessage) {
+        return new Result<Void>()
+                .setCode(errorCode)
+                .setMessage(errorMessage);
+    }
+}
