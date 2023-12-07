3731fc67a19453e741c2bf37bb40374e59821028
linqi <wkw4399xs@gmail.com>
2023-12-07 15:17:45 +0800

    The 84th commit(feature): Admin 模块 Optimize user persistence layer base properties.(Project 优化用户持久层基础属性.)

--- admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/UserDO.java
@@ -1,11 +1,8 @@
 package com.nageoffer.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.nageoffer.shortlink.admin.common.database.BaseDO;
 import lombok.Data;
-
-import java.util.Date;
 
 /**
  * @ClassName UserDO
@@ -16,7 +13,7 @@ import java.util.Date;
  */
 @Data
 @TableName("t_user")
-public class UserDO {
+public class UserDO extends BaseDO {
 
     /**
      * id
@@ -52,23 +49,7 @@ public class UserDO {
      */
     private Long deletionTime;
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill= FieldFill.INSERT)
-    private Date createTime;
 
-    /**
-     * 修改时间
-     */
-    @TableField(fill= FieldFill.INSERT)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0:未删除，1：已删除
-     */
-    @TableField(fill= FieldFill.INSERT)
-    private int delFlag;
 
 
 }
