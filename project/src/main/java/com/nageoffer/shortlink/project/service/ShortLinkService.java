1e42e0150389682ba716c225b5db5b041b2d47e5
linqi <wkw4399xs@gmail.com>
2023-12-16 22:21:36 +0800

    The 100th commit(feature): Project module:Add open source request headers.(Project 模块 service 短链接接口层 初始化.)

---
+++ project/src/main/java/com/nageoffer/shortlink/project/service/ShortLinkService.java
@@ -0,0 +1,11 @@
+package com.nageoffer.shortlink.project.service;
+
+/**
+ * @InterfaceName ShortLinkService
+ * @Description 短链接接口层
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/12/16
+ */
+public interface ShortLinkService {
+}
