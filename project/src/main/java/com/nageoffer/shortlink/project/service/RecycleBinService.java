e41917d95f2abc785bec5551de7db2b71d17c609
linqi <wkw4399xs@gmail.com>
2023-12-16 22:21:02 +0800

    The 100th commit(feature): Project module:Add open source request headers.(Project 模块 service 回收站管理接口层 初始化.)

---
+++ project/src/main/java/com/nageoffer/shortlink/project/service/RecycleBinService.java
@@ -0,0 +1,11 @@
+package com.nageoffer.shortlink.project.service;
+
+/**
+ * @InterfaceName RecycleBinService
+ * @Description 回收站管理接口层
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2023/12/16
+ */
+public interface RecycleBinService {
+}
