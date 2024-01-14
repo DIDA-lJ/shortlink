6800c4c3a156685f23d9dc34cb0f2d2b180e710d
linqi <wkw4399xs@gmail.com>
2024-01-14 14:29:12 +0800

    The 100th commit(feature): Admin module:Redis configuration changes.(Admin 模块 短链接风控脚本书写)

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserFlowRiskControlFilter.java
@@ -0,0 +1,16 @@
+package com.nageoffer.shortlink.admin.common.biz.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description  用户操作流量风控过滤器
+ */
+
+@Slf4j
+@RequiredArgsConstructor
+public class UserFlowRiskControlFilter {
+
+}
