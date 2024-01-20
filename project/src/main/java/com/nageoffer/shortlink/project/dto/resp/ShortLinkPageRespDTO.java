604bea23ed0ba7b14dc5d9d666bbdaa45b85f4b1
linqi <wkw4399xs@gmail.com>
2024-01-20 21:09:28 +0800

    The 108th commit(feature):Creates an entity that returns a parameter about the short link page..

---
+++ project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -0,0 +1,103 @@
+package com.nageoffer.shortlink.project.dto.resp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 短链接分页返回参数
+ */
+
+@Data
+public class ShortLinkPageRespDTO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 域名
+     */
+    private String domain;
+
+    /**
+     * 短链接
+     */
+    private String shortUri;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 有效期类型 0：永久有效 1：自定义
+     */
+    private Integer validDateType;
+
+    /**
+     * 有效期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    /**
+     * 描述
+     */
+    private String describe;
+
+    /**
+     * 网站标识
+     */
+    private String favicon;
+
+    /**
+     * 历史PV
+     */
+    private Integer totalPv;
+
+    /**
+     * 今日PV
+     */
+    private Integer todayPv;
+
+    /**
+     * 历史UV
+     */
+    private Integer totalUv;
+
+    /**
+     * 今日UV
+     */
+    private Integer todayUv;
+
+    /**
+     * 历史UIP
+     */
+    private Integer totalUip;
+
+    /**
+     * 今日UIP
+     */
+    private Integer todayUip;
+}
