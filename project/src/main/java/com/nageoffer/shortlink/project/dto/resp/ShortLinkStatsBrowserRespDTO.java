4d1f229c5f9a88bc73dee1d883bbfe55176e6756
linqi <wkw4399xs@gmail.com>
2024-01-20 23:03:13 +0800

    The 111th commit(feature):Creates an entity for short link browser monitoring response parameters.

---
+++ project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkStatsBrowserRespDTO.java
@@ -0,0 +1,34 @@
+package com.nageoffer.shortlink.project.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 短链接浏览器监控响应参数
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkStatsBrowserRespDTO {
+
+    /**
+     * 统计
+     */
+    private Integer cnt;
+
+    /**
+     * 浏览器
+     */
+    private String browser;
+
+    /**
+     * 占比
+     */
+    private Double ratio;
+}
