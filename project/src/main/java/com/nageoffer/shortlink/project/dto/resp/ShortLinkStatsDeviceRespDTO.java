9280d11623965496e2663f6280f6aa2b66672825
linqi <wkw4399xs@gmail.com>
2024-01-20 23:04:21 +0800

    The 112th commit(feature):Creates an entity for monitoring response parameters about short link access devices.

---
+++ project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkStatsDeviceRespDTO.java
@@ -0,0 +1,34 @@
+package com.nageoffer.shortlink.project.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 短链接访问设备监控响应参数
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkStatsDeviceRespDTO {
+
+    /**
+     * 统计
+     */
+    private Integer cnt;
+
+    /**
+     * 设备类型
+     */
+    private String device;
+
+    /**
+     * 占比
+     */
+    private Double ratio;
+}
