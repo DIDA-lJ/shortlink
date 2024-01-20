070d068839ab127d8dc3187fb73bb20371dbe353
linqi <wkw4399xs@gmail.com>
2024-01-20 23:16:52 +0800

    The 112th commit(feature):Creates an entity that monitors response parameters for the short link region.

---
+++ project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkStatsLocaleCNRespDTO.java
@@ -0,0 +1,34 @@
+package com.nageoffer.shortlink.project.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 短链接地区监控响应参数
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkStatsLocaleCNRespDTO {
+
+    /**
+     * 统计
+     */
+    private Integer cnt;
+
+    /**
+     * 地区
+     */
+    private String locale;
+
+    /**
+     * 占比
+     */
+    private Double ratio;
+}
