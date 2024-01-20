db2064d9013384eaf522130834849c83e26b5c70
linqi <wkw4399xs@gmail.com>
2024-01-20 23:20:20 +0800

    The 113th commit(feature): Creates an entity about the short-link operating system monitoring response parameters.parameters.

---
+++ project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkStatsOsRespDTO.java
@@ -0,0 +1,34 @@
+package com.nageoffer.shortlink.project.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 短链接操作系统监控响应参数
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkStatsOsRespDTO {
+
+    /**
+     * 统计
+     */
+    private Integer cnt;
+
+    /**
+     * 操作系统
+     */
+    private String os;
+
+    /**
+     * 占比
+     */
+    private Double ratio;
+}
