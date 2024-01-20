2ff5127b1f1d11585332708931301d69f7e3c7df
linqi <wkw4399xs@gmail.com>
2024-01-20 21:05:57 +0800

    The 105th commit(feature): Create an entity class for creating response objects in batches about short links.

---
+++ project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkBatchCreateRespDTO.java
@@ -0,0 +1,31 @@
+package com.nageoffer.shortlink.project.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 短链接批量创建响应对象
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkBatchCreateRespDTO {
+
+    /**
+     * 成功数量
+     */
+    private Integer total;
+
+    /**
+     * 批量创建返回参数
+     */
+    private List<ShortLinkBaseInfoRespDTO> baseLinkInfos;
+}
