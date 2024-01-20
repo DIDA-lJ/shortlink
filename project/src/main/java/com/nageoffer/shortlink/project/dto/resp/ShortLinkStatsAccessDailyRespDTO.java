7855441e1d1c514528b8b198e46113290ff7d2e0
linqi <wkw4399xs@gmail.com>
2024-01-20 21:10:32 +0800

    The 109th commit(feature):Creates an entity class for the short link base access monitoring response parameter.

---
+++ project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkStatsAccessDailyRespDTO.java
@@ -0,0 +1,39 @@
+package com.nageoffer.shortlink.project.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 短链接基础访问监控响应参数
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkStatsAccessDailyRespDTO {
+
+    /**
+     * 日期
+     */
+    private String date;
+
+    /**
+     * 访问量
+     */
+    private Integer pv;
+
+    /**
+     * 独立访客数
+     */
+    private Integer uv;
+
+    /**
+     * 独立IP数
+     */
+    private Integer uip;
+}
