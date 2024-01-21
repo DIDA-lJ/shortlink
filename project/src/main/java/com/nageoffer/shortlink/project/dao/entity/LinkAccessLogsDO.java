2b25025715937ccaf6809b2cf7efc41d931c1d43
linqi <wkw4399xs@gmail.com>
2024-01-21 22:02:08 +0800

    The 114th commit(feature): indicates the configuration of the short link system Project module.

---
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/entity/LinkAccessLogsDO.java
@@ -0,0 +1,10 @@
+package com.nageoffer.shortlink.project.dao.entity;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description
+ */
+
+public class LinkAccessLogsDO {
+}
---
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortLinkDO.java
@@ -0,0 +1,130 @@
+package com.nageoffer.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.nageoffer.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 短链接分组实体
+ */
+
+@Data
+@Builder
+@TableName("t_link")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 域名
+     */
+    private String domain;
+
+    /**
+     * 短链接
+     */
+    private String shortUri;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+    /**
+     * 点击量
+     */
+    private Integer clickNum;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 启用标识 0：启用 1：未启用
+     */
+    private Integer enableStatus;
+
+    /**
+     * 创建类型 0：接口创建 1：控制台创建
+     */
+    private Integer createdType;
+
+    /**
+     * 有效期类型 0：永久有效 1：自定义
+     */
+    private Integer validDateType;
+
+    /**
+     * 有效期
+     */
+    private Date validDate;
+
+    /**
+     * 描述
+     */
+    @TableField("`describe`")
+    private String describe;
+
+    /**
+     * 网站标识
+     */
+    private String favicon;
+
+    /**
+     * 历史PV
+     */
+    private Integer totalPv;
+
+    /**
+     * 历史UV
+     */
+    private Integer totalUv;
+
+    /**
+     * 历史UIP
+     */
+    private Integer totalUip;
+
+    /**
+     * 今日PV
+     */
+    @TableField(exist = false)
+    private Integer todayPv;
+
+    /**
+     * 今日UV
+     */
+    @TableField(exist = false)
+    private Integer todayUv;
+
+    /**
+     * 今日UIP
+     */
+    @TableField(exist = false)
+    private Integer todayUip;
+
+    /**
+     * 删除时间
+     */
+    private Long delTime;
+}
+
